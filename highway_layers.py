817eeaa5bedb430a3312b438b7a75ab2f1da1535
c0nn3r <connervercellino@gmail.com>
2017-02-14 23:23:30 -0800

    Reorganized function and added gate activation argument.

--- highway_layers.py
+++ highway_layers.py
@@ -7,21 +7,23 @@ class HighwayMLP(nn.Module):
     def __init__(self,
                  input_size,
                  gate_bias=-2,
-                 activation_function=nn.functional.elu):
+                 activation_function=nn.functional.elu,
+                 gate_activation=nn.functional.softmax):
 
         super(HighwayMLP, self).__init__()
 
         self.activation_function = activation_function
+        self.gate_activation = gate_activation
+
+        self.normal_layer = nn.Linear(input_size, input_size)
 
         self.gate_layer = nn.Linear(input_size, input_size)
         self.gate_layer.bias.data[:] = gate_bias
 
-        self.normal_layer = nn.Linear(input_size, input_size)
-
     def forward(self, x):
 
         normal_layer_result = self.activation_function(self.normal_layer(x))
-        gate_layer_result = nn.functional.softmax(self.gate_layer(x))
+        gate_layer_result = self.gate_activation(self.gate_layer(x))
 
         multiplyed_gate_and_normal = torch.mul(normal_layer_result, gate_layer_result)
         multiplyed_gate_and_input = torch.mul((1 - gate_layer_result), x)
