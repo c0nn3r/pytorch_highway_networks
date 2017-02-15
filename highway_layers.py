8132044431d430f0dc62cd8385ebec5703f2c497
c0nn3r <connervercellino@gmail.com>
2017-02-14 22:49:17 -0800

    Changed default activation and fixed implementation mistake.

--- highway_layers.py
+++ highway_layers.py
@@ -7,11 +7,10 @@ class HighwayMLP(nn.Module):
     def __init__(self,
                  input_size,
                  gate_bias=-2,
-                 activation_function=nn.ReLU()):
+                 activation_function=nn.functional.elu()):
 
         super(HighwayMLP, self).__init__()
 
-        self.softmax = nn.Softmax()
         self.activation_function = activation_function
 
         self.gate_layer = nn.Linear(input_size, input_size)
@@ -22,7 +21,7 @@ class HighwayMLP(nn.Module):
     def forward(self, x):
 
         normal_layer_result = self.activation_function(self.normal_layer(x))
-        gate_layer_result = self.softmax(self.gate_layer(x))
+        gate_layer_result = nn.functional.sigmoid(self.gate_layer(x))
 
         multiplyed_gate_and_normal = torch.mul(normal_layer_result, gate_layer_result)
         multiplyed_gate_and_input = torch.mul((1 - gate_layer_result), x)
